42a06ca41632afeab6aaaedf2e087774201d1357
Svitlana Andrushenko <wwwli4e@gmail.com>
2024-08-11 08:46:54 +0300

    feat: all tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        let nReverse = Math.abs(n).toString().split('').reverse();
+        return Number(nReverse.join(''));
+    } else if (n % 10 === 0 ) {
+        let nReverse = (n / 10).toString().split('').reverse();
+        return Number(nReverse.join(''));
+    } else {
+        let nReverse = n.toString().split('').reverse();
+        return Number(nReverse.join(''));
+    }
+
 }
